46f3b9903a80085e086aceed576149bbdbc331c8
Илья Рудоманов <i.rusumanov@profsoft.pro>
2023-10-31 17:17:48 +0400

    changes

---
+++ Lesson_3_Rudomanov/app/src/main/java/com/example/lesson_3_rudomanov/Profile.kt
@@ -0,0 +1,11 @@
+package com.example.lesson_3_rudomanov
+
+class Profile(
+    val id: Long,
+    val speciality: String,
+    val name: String,
+    val surname: String,
+    val email: String,
+    val login: String,
+    val region: String,
+)
--- Lesson_3_Rudomanov/app/src/main/java/com/example/lesson_3_rudomanov/SecondActivity.kt
+++ Lesson_3_Rudomanov/app/src/main/java/com/example/lesson_3_rudomanov/SecondActivity.kt
@@ -2,34 +2,67 @@ package com.example.lesson_3_rudomanov
 
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.lesson_3_rudomanov.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
-
+    private val binding by lazy { ActivitySecondBinding.inflate(LayoutInflater.from(this)) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        setContentView(binding.root)
+        binding.toolbar.setNavigationOnClickListener {
+            Toast.makeText(this, "Выход", Toast.LENGTH_SHORT).show()
+        }
+        binding.toolbar.menu.findItem(R.id.editProfile).setOnMenuItemClickListener {
+            Toast.makeText(this, "Изменение профиля", Toast.LENGTH_SHORT).show()
+            true
+        }
 
-        findViewById<TextView>(R.id.textViewPersonal).text = "Личные данные"
+        val person = Profile(
+            7898769,
+            "Специалист",
+            "Анастасия",
+            "Антонина",
+            "anykee.box@gmail.ru",
+            "HIE023UOI88",
+            "Санкт-Петербург"
+        )
 
-        findViewById<View>(R.id.nameInfo).findViewById<TextView>(R.id.textViewTextOne).text = "Name"
-        findViewById<View>(R.id.nameInfo).findViewById<TextView>(R.id.textViewTextTwo).text = "Анастасия"
+        findViewById<TextView>(R.id.textViewCard).text =
+            "${getString(R.string.card_number)}${person.id}"
 
-        findViewById<View>(R.id.surnameInfo).findViewById<TextView>(R.id.textViewTextOne).text = "Фамилия"
-        findViewById<View>(R.id.surnameInfo).findViewById<TextView>(R.id.textViewTextTwo).text = "Антонина"
+        findViewById<TextView>(R.id.textViewProf).text = person.speciality
 
-        findViewById<View>(R.id.mailInfo).findViewById<TextView>(R.id.textViewTextOne).text = "E-mail"
-        findViewById<View>(R.id.mailInfo).findViewById<TextView>(R.id.textViewTextTwo).text = "anykee.box@gmail.ru"
 
-        findViewById<View>(R.id.logInfo).findViewById<TextView>(R.id.textViewTextOne).text = "Логин"
-        findViewById<View>(R.id.logInfo).findViewById<TextView>(R.id.textViewTextTwo).text = "HIE023UOI88"
+        findViewById<View>(R.id.nameInfo).findViewById<TextView>(R.id.textViewTextOne).text =
+            getString(R.string.name)
+        findViewById<View>(R.id.nameInfo).findViewById<TextView>(R.id.textViewTextTwo).text =
+            person.name
 
-        findViewById<View>(R.id.regInfo).findViewById<TextView>(R.id.textViewTextOne).text = "Ваш регион"
-        findViewById<View>(R.id.regInfo).findViewById<TextView>(R.id.textViewTextTwo).text = "Санкт-Петербург"
+        findViewById<View>(R.id.surnameInfo).findViewById<TextView>(R.id.textViewTextOne).text =
+            getString(R.string.surname)
+        findViewById<View>(R.id.surnameInfo).findViewById<TextView>(R.id.textViewTextTwo).text =
+            person.surname
+
+        findViewById<View>(R.id.mailInfo).findViewById<TextView>(R.id.textViewTextOne).text =
+            getString(R.string.email)
+        findViewById<View>(R.id.mailInfo).findViewById<TextView>(R.id.textViewTextTwo).text =
+            person.email
+
+        findViewById<View>(R.id.logInfo).findViewById<TextView>(R.id.textViewTextOne).text =
+            getString(R.string.login)
+        findViewById<View>(R.id.logInfo).findViewById<TextView>(R.id.textViewTextTwo).text =
+            person.login
+
+        findViewById<View>(R.id.regInfo).findViewById<TextView>(R.id.textViewTextOne).text =
+            getString(R.string.ur_region)
+        findViewById<View>(R.id.regInfo).findViewById<TextView>(R.id.textViewTextTwo).text =
+            person.region
 
         findViewById<TextView>(R.id.textViewExit).setOnClickListener {
             Toast.makeText(this, "Выход из профиля", Toast.LENGTH_SHORT).show()
